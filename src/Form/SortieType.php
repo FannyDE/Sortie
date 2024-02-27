a9a37f5166a62ada0492c7ec20bada46993f125c
Azlig <137167255+Georgi-8@users.noreply.github.com>
2024-02-27 09:57:24 +0100

    Application d'un peut de style css sur la navbar. Application de mise forme pour le formulaire de création d'une sortie.

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -4,20 +4,36 @@ namespace App\Form;
 
 use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class SortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionMax')
-            ->add('infosSortie')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie : '
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie : '
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label' => 'Date limite d\'inscription : '
+            ])
+            ->add('nbInscriptionMax', TextType::class, [
+                'label' => 'Nombre de places : '
+            ])
+            ->add('duree', TextType::class, [
+                'label' => 'Durée : '
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label' => 'Description & infos : '
+            ])
         ;
     }
 
