136aaa1e3940747b415769dcbe16da680b931a02
Comrade-Cat <onetaroyamada@gmail.com>
2024-02-27 12:03:40 +0100

    env local

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,7 +19,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
 
-    #[ORM\Column]
+    #[ORM\Column(nullable: true)]
     private ?string $pseudo = null;
 
 
